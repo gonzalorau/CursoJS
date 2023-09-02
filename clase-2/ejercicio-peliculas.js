aadaa5f1b9099e00cf1bd4a319acd36345add9f3
GonzaloR <gonzalorau@gmail.com>
2023-09-01 21:54:27 -0300

    Object USUARIO
    Creo objeto usuario con un array que contiene 5 objetos con 2 propiedades c/u

---
+++ clase-2/ejercicio-peliculas.js
@@ -0,0 +1,29 @@
+// Peliculas Favoritas:
+// Crea un objeto usuario con propiedades como nombre y peliculasFavoritas, que es un array de objetos donde cada objeto representa una película con propiedades como titulo y año. Agrega al menos tres películas favoritas y muestra en la consola el título de la segunda película favorita.
+
+const USUARIO = {
+nombre: "Gonzalo",
+peliculasFavoritas: [
+    pelicula1 = {
+        titulo: "Star Trek",
+        anio: 2016
+    },
+    pelicula2 = {
+        titulo: "Mad Max",
+        anio: 2015
+    },
+    pelicula3 = {
+        titulo: "Indiana Jones",
+        anio: 2008
+    },
+    palicula4 = {
+        titulo: "Harry Potter",
+        anio: 2001
+    },
+    pelicula5 = {
+        titulo: "Batman",
+        anio: 2012
+    }
+]
+}
+
