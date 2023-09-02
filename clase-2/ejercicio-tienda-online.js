1edc92dea9a53d719912fa2a9246b58280ee95ef
GonzaloR <gonzalorau@gmail.com>
2023-09-01 22:18:03 -0300

    actualizo el ejercicio añadiendo el objeto producto

--- clase-2/ejercicio-tienda-online.js
+++ clase-2/ejercicio-tienda-online.js
@@ -1,3 +1,9 @@
 // Tienda en Línea:
 // Crea un objeto producto con propiedades como nombre, precio y disponible. Luego, crea una función llamada mostrarProducto que tome un objeto producto como argumento y muestre en la consola toda la información del producto.
 
+const PRODUCTO = {
+    nombre: "Velador",
+    precio: 900,
+    disponible: true
+}
+
