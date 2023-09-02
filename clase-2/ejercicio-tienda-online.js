d657eb4c05ca4096779e059b07ae31a859059247
GonzaloR <gonzalorau@gmail.com>
2023-09-01 22:17:13 -0300

    Creo el ejercicio de tienda online

---
+++ clase-2/ejercicio-tienda-online.js
@@ -0,0 +1,3 @@
+// Tienda en Línea:
+// Crea un objeto producto con propiedades como nombre, precio y disponible. Luego, crea una función llamada mostrarProducto que tome un objeto producto como argumento y muestre en la consola toda la información del producto.
+
